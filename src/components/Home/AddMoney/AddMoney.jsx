1827528a8307a7eeb7c59fb193839b1431b7651e
CoderNehal <nehalughade1221@gmai.com>
2021-11-04 00:48:39 +0530

    Added animation in home and add money

--- src/components/Home/AddMoney/AddMoney.jsx
+++ src/components/Home/AddMoney/AddMoney.jsx
@@ -1,13 +1,45 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './AddMoney.scss';
 import Cash from '../../../images/cash.png';
 import PassiveIncome from '../../../images/PassiveIncome.png';
 import Other from '../../../images/Other.png';
+import { useInView } from 'react-intersection-observer';
+import { motion, useAnimation } from 'framer-motion';
+import AnimatedNumber from 'animated-number-react';
+const container = {
+	hidden: { opacity: 1, scale: 0 },
+	visible: {
+		opacity: 1,
+		scale: 1,
+		transition: {
+			delayChildren: 0.3,
+			staggerChildren: 0.2,
+		},
+	},
+};
+
+const item = {
+	hidden: { y: 20, opacity: 0 },
+	visible: {
+		y: 0,
+		opacity: 1,
+	},
+};
 const AddMoney = () => {
 	const [CurrentBalance, setCurrentBalance] = useState(34680); //Fetch here
 	const [UpdatedBalance, setUpdatedBalance] = useState(34680);
 	const [AmoutToAdd, setAmoutToAdd] = useState(0);
 	const [ModeOFIncome, setModeOFIncome] = useState(null);
+	const { ref, inView } = useInView({
+		threshold: 1,
+	});
+	const animation = useAnimation();
+	useEffect(() => {
+		if (inView) {
+			
+			animation.start({ rotate: 0, scale: 1 });
+		}
+	}, [inView]);
 	const handleIncomeSource = (classList, id) => {
 		document.getElementById('Salary').classList.remove('border-primary');
 		document.getElementById('Passive').classList.remove('border-primary');
@@ -16,6 +48,7 @@ const AddMoney = () => {
 		console.log(id);
 		setModeOFIncome(id);
 	};
+	const formatValue = (value) => `₹ ${Number(value).toLocaleString()}`;
 	const HandleAddMoney = () => {
 		if (AmoutToAdd === 0) {
 			alert('Please Enter a Valid Amount');
@@ -40,14 +73,32 @@ const AddMoney = () => {
 		<div className='AddMoneyContainer p-8 '>
 			<div className='flex flex-col md:flex-row justify-between h-full items-center '>
 				<div className=' w-1/2 h-full flex flex-col'>
-					<div className='currentBalance h-1/2 text-4xl md:text-6xl flex flex-col justify-center items-center text-white'>
+					<motion.div
+						initial={{ opacity: 0, x: -50 }}
+						transition={{ duration: 1 }}
+						animate={{ opacity: 0.7, x: 0 }}
+						className='currentBalance h-1/2 text-4xl md:text-6xl flex flex-col justify-center items-center text-white'>
 						₹ {CurrentBalance.toLocaleString()}
-						<p className='text-base md:text-lg pb-4 md:pt-4 md:pb-0 text-center'>Current Balance</p>
-					</div>
-					<div className='updatedBalance h-1/2 text-4xl md:text-6xl flex flex-col justify-center items-center text-green'>
-						₹ {UpdatedBalance.toLocaleString()}
-						<p className='text-base md:text-lg pb-4 md:pt-4 md:pb-0 text-center'>Balance After Adding Money</p>
-					</div>
+						<p className='text-base md:text-lg pb-4 md:pt-4 md:pb-0 text-center'>
+							Current Balance
+						</p>
+					</motion.div>
+					<motion.div
+						initial={{ opacity: 0, x: 50 }}
+						transition={{
+							duration: 1,
+						}}
+						animate={{ opacity: 1, x: 0 }}
+						className='updatedBalance h-1/2 text-4xl md:text-6xl flex flex-col justify-center items-center text-green'>
+						<AnimatedNumber
+							value={UpdatedBalance}
+							formatValue={formatValue}
+							duration={250}
+						/>
+						<p className='text-base md:text-lg pb-4 md:pt-4 md:pb-0 text-center'>
+							Balance After Adding Money
+						</p>
+					</motion.div>
 				</div>
 				<div className=' w-full md:w-1/2 px-0 py-3 md:px-16 h-full '>
 					<div className='border h-full lg:w-11/12  flex flex-col bg-white text-black rounded-md'>
@@ -66,71 +117,92 @@ const AddMoney = () => {
 									/>
 								</div>
 							</div>
-							<div className='MoneyOptions flex justify-between items-center w-3/4 h-60 lg:h-1/2 flex-wrap px-10 s' >
-								<div
+							<motion.div
+								variants={container}
+								initial='hidden'
+								animate='visible'
+								className='MoneyOptions flex justify-between items-center w-3/4 h-60 lg:h-1/2 flex-wrap px-10 s'>
+								<motion.div
+									variants={item}
 									className='py-1 px-3 cursor-pointer rounded-md border-2 text-lg border-primary  text-primary'
 									onClick={(e) => {
 										setAmoutToAdd(100);
 									}}>
 									₹ 100
-								</div>
-								<div
+								</motion.div>
+								<motion.div
+									variants={item}
 									className='py-1 px-3 cursor-pointer rounded-md border-2 text-lg border-primary  text-primary'
 									onClick={(e) => {
 										setAmoutToAdd(200);
 									}}>
 									₹ 200
-								</div>
-								<div
+								</motion.div>
+								<motion.div
+									variants={item}
 									className='py-1 px-3 cursor-pointer rounded-md border-2 text-lg border-primary  text-primary'
 									onClick={(e) => {
 										setAmoutToAdd(400);
 									}}>
 									₹ 400
-								</div>
-								<div
+								</motion.div>
+								<motion.div
+									variants={item}
 									className='py-1 px-3 cursor-pointer rounded-md border-2 text-lg border-primary  text-primary'
 									onClick={(e) => {
 										setAmoutToAdd(800);
 									}}>
 									₹ 800
-								</div>
-								<div
+								</motion.div>
+								<motion.div
+									variants={item}
 									className='py-1 px-2 cursor-pointer rounded-md border-2 text-lg border-primary  text-primary'
 									onClick={(e) => {
 										setAmoutToAdd(1200);
 									}}>
 									₹ 1200
-								</div>
-								<div
+								</motion.div>
+								<motion.div
+									variants={item}
 									className='py-1 px-2 cursor-pointer rounded-md border-2 text-lg border-primary  text-primary'
 									onClick={(e) => {
 										setAmoutToAdd(1600);
 									}}>
 									₹ 1600
-								</div>
-								<div
+								</motion.div>
+								<motion.div
+									variants={item}
 									className='py-1 px-2 cursor-pointer rounded-md border-2 text-lg border-primary  text-primary'
 									onClick={(e) => {
 										setAmoutToAdd(2000);
 									}}>
 									₹ 2000
-								</div>
-								<div
+								</motion.div>
+								<motion.div
+									variants={item}
 									className='py-1 px-2 cursor-pointer rounded-md border-2 text-lg border-primary  text-primary'
 									onClick={(e) => {
 										setAmoutToAdd(2500);
 									}}>
 									₹ 2500
-								</div>
-							</div>
+								</motion.div>
+							</motion.div>
 						</div>
 						<div className='lowerPart h-1/2 flex flex-col justify-between items-center py-3'>
 							<div className='text-green text-lg lg:text-xl'>
 								What is the source of income?
 							</div>
-							<div className='lg:px-8 py-6 flex h-full w-3/4 justify-between '>
-								<div
+							<motion.div
+								ref={ref}
+								className='lg:px-8 py-6 flex h-full w-3/4 justify-between '>
+								<motion.div
+									initial={{ scale: 0, rotate: 180 }}
+									animate={animation}
+									transition={{
+										type: 'spring',
+										stiffness: 260,
+										damping: 40,
+									}}
 									className=' h-28 lg:h-full w-20 lg:w-1/4 border-2  flex flex-col items-center justify-center py-2 px-3 '
 									id='Salary'
 									onClick={(e) => {
@@ -143,8 +215,15 @@ const AddMoney = () => {
 									<p className='text-sm text-green font-semibold uppercase pt-2 '>
 										Salary
 									</p>
-								</div>
-								<div
+								</motion.div>
+								<motion.div
+									initial={{ scale: 0, rotate: 180 }}
+									animate={animation}
+									transition={{
+										type: 'spring',
+										stiffness: 260,
+										damping: 40,
+									}}
 									className=' h-28 lg:h-full w-20 lg:w-1/4 border-2  flex flex-col items-center justify-center py-2 px-3 '
 									id='Passive'
 									onClick={(e) => {
@@ -157,8 +236,15 @@ const AddMoney = () => {
 									<p className='text-sm text-green font-semibold uppercase pt-2 '>
 										Paasive
 									</p>
-								</div>
-								<div
+								</motion.div>
+								<motion.div
+									initial={{ scale: 0, rotate: 180 }}
+									animate={animation}
+									transition={{
+										type: 'spring',
+										stiffness: 260,
+										damping: 40,
+									}}
 									className=' h-28 lg:h-full w-20 lg:w-1/4 border-2  flex flex-col items-center justify-center py-2 px-3 '
 									id='Other'
 									onClick={(e) => {
@@ -171,16 +257,22 @@ const AddMoney = () => {
 									<p className='text-sm text-green font-semibold uppercase pt-2 '>
 										Other
 									</p>
-								</div>
-							</div>
-							<div className='w-full  pb-3 flex justify-center items-center'>
+								</motion.div>
+							</motion.div>
+							<motion.div
+								whileHover={{
+									scale: 1.1,
+									transition: { duration: 0.3 },
+								}}
+								whileTap={{ scale: 0.9 }}
+								className='w-full  pb-3 flex justify-center items-center'>
 								<input
 									type='button'
 									className='w-10/12 py-2 rounded-lg bg-primary text-white text-xl cursor-pointer'
 									value='Add funds'
 									onClick={HandleAddMoney}
 								/>
-							</div>
+							</motion.div>
 						</div>
 					</div>
 				</div>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,35 +1,74 @@
 import React, { useState } from 'react';
 import './Home.scss';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 const Home = () => {
 	const [Balance, setBalance] = useState(34680);
 	const [SpentLastMonth, setSpentLastMonth] = useState(123443);
 	const [SpentLastYear, setSpentLastYear] = useState(12354443);
 	return (
 		<div className=' w-screen h-auto cursor-default  relative'>
-			<div className='currentAmountBox mt-6 md:mt-16 rounded-md mx-auto w-11/12 border flex flex-col justify-between items-center font-semibold  bg-white py-3 border-gray-500  '>
+			<motion.div
+				initial={{ width: 0 }}
+				animate={{ width: '91.666667%' }}
+				transition={{ duration: 0.5 }}
+				className='currentAmountBox mt-6 md:mt-16 rounded-md mx-auto w-11/12 border flex flex-col justify-between items-center font-semibold  bg-white py-3 border-gray-500  '>
 				<div className='BlankDiv'></div>
-				<div>
-					<div className='amount text-4xl  border-b-2  md:text-8xl  text-green px-5 md:px-16'>
+				<motion.div
+					initial={{ y: -30, opacity: 0 }}
+					animate={{ y: 0, opacity: 1 }}
+					transition={{ duration: 0.7, delay: 0.3 }}>
+					<div className='amount text-4xl    md:text-8xl  text-green px-5 md:px-16'>
 						₹ {Balance.toLocaleString()}
 					</div>
+					<motion.hr
+						initial={{ width: 0 }}
+						animate={{ width: '100%' }}
+						transition={{ duration: 0.5, delay: 0.7 }}
+						style={{ height: '1.5px', background: '#eeeeee' }}
+					/>
 					<p className='text-lg text-center  text-green pt-3'>Total Balance</p>
-				</div>
+				</motion.div>
 				<div className='flex justify-between w-full md:w-3/4'>
-					<div className='lastMonth text-primary text-md md:text-xl  text-center p-4'>
-						<p className='border-b-2 mb-2 px-1 md:px-8'>
-							Total Spent Last Month
-						</p>
-						₹ {SpentLastMonth.toLocaleString()}
-					</div>
-					<div className='totalThisYear text-md md:text-xl   text-center p-4 '>
-						<p className='border-b-2 mb-2 px-1 md:px-8'>
-							Total Spent This Year
-						</p>
-						₹ {SpentLastYear.toLocaleString()}
-					</div>
+					<motion.div
+						initial={{ y: 30, opacity: 0 }}
+						animate={{ y: 0, opacity: 1 }}
+						transition={{ duration: 0.7, delay: 1.3 }}
+						className='lastMonth text-primary text-md md:text-xl text-center p-4'>
+						<p className='  px-1 md:px-8'>Total Spent Last Month</p>
+						<motion.hr
+							className='mb-2'
+							initial={{ width: 0 }}
+							animate={{ width: '100%' }}
+							transition={{ duration: 0.3, delay: 2 }}
+							style={{
+								height: '1.5px',
+								background: '#eeeeee',
+							}}
+						/>
+						₹{SpentLastMonth.toLocaleString()}
+					</motion.div>
+
+					<motion.div
+						initial={{ y: 30, opacity: 0 }}
+						animate={{ y: 0, opacity: 1 }}
+						transition={{ duration: 0.7, delay: 1.3 }}
+						className='totalThisYear text-md md:text-xl text-center p-4 '>
+						<p className=' px-1 md:px-8'>Total Spent This Year</p>
+						<motion.hr
+							className='mb-2'
+							initial={{ width: 0 }}
+							animate={{ width: '100%' }}
+							transition={{ duration: 0.3, delay: 2 }}
+							style={{
+								height: '1.5px',
+								background: '#eeeeee',
+							}}
+						/>
+						₹{SpentLastYear.toLocaleString()}
+					</motion.div>
 				</div>
-			</div>
+			</motion.div>
 
 			<div className='buttons flex flex-col px-12 lg:px-0 lg:flex-row justify-between items-center w-11/12 mx-auto mt-4 text-white h-52  '>
 				{/* <button className='rounded text-lg btn-border-1 px-36 py-0 xl:py-4 mt-4 xl:mt-0   transition duration-200 ease-in bg-green'>
@@ -41,62 +80,86 @@ const Home = () => {
 				<button className='rounded text-lg btn-border-1 px-36 py-0 xl:py-4 mt-4 xl:mt-0 transition duration-200 ease-in bg-yellow-300'>
 					View History
 				</button> */}
-				<Link
-					class='btn btn-border-1 border-none outline-none px-36 py-0 xl:py-4 mt-4 xl:mt-0 rounded border-gray-300 text-lg text-center relative flex justify-center items-center text-white bg-green '
-					to='/add-money'>
-					<svg
-						xmlns='http://www.w3.org/2000/svg'
-						className='h-6 w-6'
-						fill='none'
-						viewBox='0 0 24 24'
-						stroke='currentColor'>
-						<path
-							strokeLinecap='round'
-							strokeLinejoin='round'
-							strokeWidth={2}
-							d='M12 4v16m8-8H4'
-						/>
-					</svg>
-					<span className='ml-6 md:ml-3'>Add Money</span>
-				</Link>
-				<Link
-					class='btn btn-border-1  px-36 py-0 xl:py-4 mt-4 xl:mt-0 rounded border-gray-300 text-lg text-center relative flex justify-center items-center text-white bg-red'
-					to='/spend-money'>
-					<svg
-						xmlns='http://www.w3.org/2000/svg'
-						className='h-6 w-6'
-						fill='none'
-						viewBox='0 0 24 24'
-						stroke='currentColor'>
-						<path
-							strokeLinecap='round'
-							strokeLinejoin='round'
-							strokeWidth={2}
-							d='M6 18L18 6M6 6l12 12'
-						/>
-					</svg>
+				<motion.div
+					initial={{ x:'-100vw' }}
+					animate={{ x:0 }}
+					transition={{
+						duration:0.7,
+						delay: 2,
+					}}>
+					<Link
+						class='btn btn-border-1 border-none outline-none px-36 py-0 xl:py-4 mt-4 xl:mt-0 rounded border-gray-300 text-lg text-center relative flex justify-center items-center text-white bg-green '
+						to='/add-money'>
+						<svg
+							xmlns='http://www.w3.org/2000/svg'
+							className='h-6 w-6'
+							fill='none'
+							viewBox='0 0 24 24'
+							stroke='currentColor'>
+							<path
+								strokeLinecap='round'
+								strokeLinejoin='round'
+								strokeWidth={2}
+								d='M12 4v16m8-8H4'
+							/>
+						</svg>
+						<span className='ml-6 md:ml-3'>Add Money</span>
+					</Link>
+				</motion.div>
+				<motion.div
+					initial={{ x:'-100vw' }}
+					animate={{ x:0 }}
+					transition={{
+						duration:1,
+						delay: 2,
+					}}>
+					<Link
+						class='btn btn-border-1  px-36 py-0 xl:py-4 mt-4 xl:mt-0 rounded border-gray-300 text-lg text-center relative flex justify-center items-center text-white bg-red'
+						to='/spend-money'>
+						<svg
+							xmlns='http://www.w3.org/2000/svg'
+							className='h-6 w-6'
+							fill='none'
+							viewBox='0 0 24 24'
+							stroke='currentColor'>
+							<path
+								strokeLinecap='round'
+								strokeLinejoin='round'
+								strokeWidth={2}
+								d='M6 18L18 6M6 6l12 12'
+							/>
+						</svg>
 
-					<span className='ml-6 md:ml-3'>Spend Money</span>
-				</Link>
-				<Link
-					class='btn btn-border-1  px-36 py-0 xl:py-4 mt-4 xl:mt-0 rounded border-gray-300 text-lg text-center relative flex justify-center items-center text-white bg-yellow-300'
-					to='/view-history'>
-					<svg
-						xmlns='http://www.w3.org/2000/svg'
-						className='h-6 w-6'
-						fill='none'
-						viewBox='0 0 24 24'
-						stroke='currentColor'>
-						<path
-							strokeLinecap='round'
-							strokeLinejoin='round'
-							strokeWidth={2}
-							d='M16 15v-1a4 4 0 00-4-4H8m0 0l3 3m-3-3l3-3m9 14V5a2 2 0 00-2-2H6a2 2 0 00-2 2v16l4-2 4 2 4-2 4 2z'
-						/>
-					</svg>
+						<span className='ml-6 md:ml-3'>Spend Money</span>
+					</Link>
+				</motion.div>
+				<motion.div
+					initial={{ x:'-100vw' }}
+					animate={{ x:0 }}
+					transition={{
+						duration:1.3,
+						delay: 2,
+					}}>
+					<Link
+						class='btn btn-border-1  px-36 py-0 xl:py-4 mt-4 xl:mt-0 rounded border-gray-300 text-lg text-center relative flex justify-center items-center text-white bg-yellow-300'
+						to='/view-history'>
+						<svg
+							xmlns='http://www.w3.org/2000/svg'
+							className='h-6 w-6'
+							fill='none'
+							viewBox='0 0 24 24'
+							stroke='currentColor'>
+							<path
+								strokeLinecap='round'
+								strokeLinejoin='round'
+								strokeWidth={2}
+								d='M16 15v-1a4 4 0 00-4-4H8m0 0l3 3m-3-3l3-3m9 14V5a2 2 0 00-2-2H6a2 2 0 00-2 2v16l4-2 4 2 4-2 4 2z'
+							/>
+						</svg>
 
-					<span className='ml-6 md:ml-3'>View History</span>
-				</Link>
+						<span className='ml-6 md:ml-3'>View History</span>
+					</Link>
+				</motion.div>
 			</div>
 			<div className='rounded-full text-white text-center w-8 absolute right-8 md:right-16 mt-12	 md:mt-0'>
 				<div class='help-tip'>
